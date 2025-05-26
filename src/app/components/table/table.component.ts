4aa691b4d9eb5f319909f7de23abd8543e399244
panhara28 <titpanhara@gmail.com>
2025-05-26 22:58:30 +0700

    add toggle checkbox

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-table',
@@ -11,15 +12,20 @@ import { FormsModule } from '@angular/forms';
 export class TableComponent implements OnChanges {
   @Input() data: any[] = [];
   @Input() pageSize = 5;
-  @Input() displayFields: string[] = []; // Fields to display from the object
-  @Input() imageFields: string[] = []; // Fields to render as <img>
-  @Input() headerLabels: { [key: string]: string } = {}; // Custom header labels
+  @Input() displayFields: string[] = [];
+  @Input() imageFields: string[] = [];
+  @Input() headerLabels: { [key: string]: string } = {};
 
+  tableTitle: string = '';
   searchTerm: string = '';
   currentPage: number = 1;
   filteredData: any[] = [];
   displayedData: any[] = [];
   headers: string[] = [];
+  selectedRows: number[] = [];
+  allSelected: boolean = false;
+
+  constructor(private router: Router) {}
 
   ngOnInit() {
     this.initializeHeaders();
@@ -69,6 +75,53 @@ export class TableComponent implements OnChanges {
   changePage(page: number) {
     this.currentPage = page;
     this.updateDisplayedData();
+  }
+
+  onEdit(id: number) {
+    this.router.navigate([`/users/${id}/edit`]);
+  }
+
+  onView(id: number) {
+    this.router.navigate([`/users/${id}`]);
+  }
+
+  onRemove(id: number) {
+    if (confirm('Are you sure you want to delete this user?')) {
+      console.log('Remove user with ID:', id);
+    }
+  }
+
+  toggleSelection(id: number) {
+    const index = this.selectedRows.indexOf(id);
+    if (index === -1) {
+      this.selectedRows.push(id);
+    } else {
+      this.selectedRows.splice(index, 1);
+    }
+    this.allSelected = this.displayedData.every((row) =>
+      this.selectedRows.includes(row.id)
+    );
+  }
+
+  isSelected(id: number): boolean {
+    return this.selectedRows.includes(id);
+  }
+
+  toggleAllSelection(event: any) {
+    this.allSelected = event.target.checked;
+    this.selectedRows = this.allSelected
+      ? this.displayedData.map((row) => row.id)
+      : [];
+  }
+
+  onBulkRemove() {
+    if (
+      confirm(
+        `Are you sure you want to remove ${this.selectedRows.length} selected users?`
+      )
+    ) {
+      console.log('Removing IDs:', this.selectedRows);
+    }
   }
 
   get totalPages(): number {
